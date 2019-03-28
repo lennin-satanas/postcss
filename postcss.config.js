a8656084c1cefee27ea3a0963443283ab1ad69dd
Lennin666 <soporte@allnexus.com.co>
2019-03-28 10:45:24 -0500

    instalando CSSNext

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,15 @@
 module.exports = {
   plugins:[
-    require('autoprefixer')({
-      grid:true
+    // require('autoprefixer')({
+    //   grid:true
+    // }),
+    require('postcss-cssnext')({
+      features:{
+        autoprefixer:{
+          grid:true,
+          flexbox:false,
+        }
+      }
     })
   ]
 
