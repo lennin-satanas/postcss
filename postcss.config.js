3e93710ef85bc1d2d98f7c18ad92cf2479aed7cb
Lennin666 <soporte@allnexus.com.co>
2019-03-28 12:43:22 -0500

    los nuevos moudulos de css y uso de variables

--- postcss.config.js
+++ postcss.config.js
@@ -3,12 +3,14 @@ module.exports = {
     // require('autoprefixer')({
     //   grid:true
     // }),
+    //prender features
     require('postcss-cssnext')({
       features:{
         autoprefixer:{
           grid:true,
           flexbox:false,
         },
+        //apagando para que lea de forma nativa
         customProperties: false,
       }
     })
